66a03c9cdfcce3430574c337f01a8810305027a2
tedj <tedj.meabiou@gmail.com>
2017-05-10 15:24:19 +0100

    create balance, get and reset all balances apis

--- src/api/controllers/balance.js
+++ src/api/controllers/balance.js
@@ -1,17 +1,46 @@
 import models from '../models';
 
-function insertBalanaceController(req, res, next) {
+function createBalanaceController(req, res, next) {
   const { account, balance } = req.body;
   models.Balance.create({
     account,
     balance,
-  }).then((resp) => {
-    res.json({ resp });
-  });
+  }).then(resp => res.json(resp))
+    .catch(err => next(err));
 }
 
 function getAllBalancesController(req, res, next) {
   models.Balance.findAll().then(balances => res.json(balances));
 }
 
-export { insertBalanaceController, getAllBalancesController };
+async function getBalanceByAccountController(req, res, next) {
+  const account = req.params.account;
+  try {
+    const balance = await models.Balance.getByAccount(account);
+    return res.json(balance);
+  } catch (err) {
+    return next(err);
+  }
+}
+
+async function resetBalancesController(req, res, next) {
+  try {
+    await models.Balance.destroy({ where: {} });
+    await Promise.all([
+      models.Balance.create({ account: 1, balance: 1000 }),
+      models.Balance.create({ account: 2, balance: 1000 }),
+      models.Balance.create({ account: 3, balance: 1000 }),
+    ]);
+    return res.json({});
+  } catch (err) {
+    return next(err);
+  }
+}
+
+
+export {
+  createBalanaceController,
+  getAllBalancesController,
+  getBalanceByAccountController,
+  resetBalancesController,
+};
--- src/api/services/router.js
+++ src/api/services/router.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 import { transferController } from '../controllers/transfer';
-import { insertBalanaceController, getAllBalancesController } from '../controllers/balance';
+import {
+  createBalanaceController,
+  getAllBalancesController,
+  getBalanceByAccountController,
+  resetBalancesController,
+} from '../controllers/balance';
 
 const router = Router();
 
 router.route('/balances/transfer').post(transferController);
-router.route('/balances').post(insertBalanaceController);
+router.route('/balances').post(createBalanaceController);
 router.route('/balances').get(getAllBalancesController);
+router.route('/balances/:account').get(getBalanceByAccountController);
+router.route('/balances/reset').post(resetBalancesController);
 
 export default router;
