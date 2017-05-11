8515f1fc905e35f9673676b4e428180594c51e96
tedj <tedj.meabiou@gmail.com>
2017-05-11 11:20:57 +0100

    clean test docker existing containers before running tests

--- src/test/test.sh
+++ src/test/test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 echo "connecting to docker-machine..."
 eval $(docker-machine env default)
+cleanup () {
+  docker-compose -p ci kill
+  docker-compose -p ci rm -f
+}
+echo "cleaning test docker containers..."
+cleanup
 echo "building containers..."
 docker-compose -p ci build --no-cache
 echo "creating containers..."
